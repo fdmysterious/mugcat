e817d44db5592ac32272173120ada30613065e3e
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2017-12-28 16:03:36 +0100

    Correcting page link to router-view so that it triggers that smooth transition :D

--- src/src/pages/fr/about.vue
+++ src/src/pages/fr/about.vue
@@ -2,7 +2,7 @@
     main-wrapper
         h1 À propos
         p Je m'appelle Florian Dupeyron, et j'ai 20 ans. Je suis actuellement étudiant en double cursus de Sciences & Musicologie à la "Sorbonne Université", fusion de Paris Sorbonne et de l'université Pierre et Marie Curie. Depuis le début de mon adolescence je me passionne d'informatique, développant surtout mon interêt pour la programmation. Je pratique également quelques instruments de musique, principalement le piano.
-        p Pour l'instant, je développe mes projets personnels, dans une volonté d'étoffer mon portfolio tout en répondant à des besoins personnels d'outils. Le plus abouti à l'heure est <a href="/projects/sten">Sten</a>, dont je me sers quasi quotidiennement.
+        p Pour l'instant, je développe mes projets personnels, dans une volonté d'étoffer mon portfolio tout en répondant à des besoins personnels d'outils. Le plus abouti à l'heure est <router-link to="/fr/projects/sten">Sten</router-link>, dont je me sers quasi quotidiennement.
 
         p Après ma double licence, je compte repartir en licence d'informatique. Le but est de valider mes compétences, tout en me laissant un laps de temps supplémentaire pour décider de la suite de mon orientation.
 
