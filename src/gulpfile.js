32e05eb860ad62f7d535ff7750636a41c4da57b2
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2021-08-01 17:00:20 +0200

    add postcss extensions + fix package json

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,21 +1,25 @@
 const { task, src, dest, watch, series, parallel } = require("gulp");
 
 const config               = require("./config.js");
+const sass                 = require("gulp-sass")(require("sass"));
+const postcss              = require("gulp-postcss");
 
-const sass                 = require("gulp-sass");
-//const postcss              = require("gulp-postcss");
-//
-//const autoprefixer         = require("autoprefixer");
-//const cssnano              = require("cssano");
-
+const autoprefixer         = require("autoprefixer");
+const cssnano              = require("cssnano");
 
 // ┌─────────────────────────────────────────┐
-// │               Watch tasks               │
+// │           CSS Transpile task            │
 // └─────────────────────────────────────────┘
 
 task("css_transpile", function() {
+    let postcss_plugins = [
+        autoprefixer(),
+        cssnano()
+    ];
+
     return src(config.path_to_src("scss/**/*.scss"))
         .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(postcss_plugins))
         .pipe(dest(config.path_to_build("css/")))
     ;
 });
